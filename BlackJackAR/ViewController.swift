c110f0f8fea45d23dfa929c5eb94fa0d48832461
petez <pete.zsolt@codeyard.eu>
2017-08-05 22:56:51 +0200

    🚧 Working on deck

--- BlackJackAR/ViewController.swift
+++ BlackJackAR/ViewController.swift
@@ -13,9 +13,13 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var planes = [OverlayPlane]()
+    var cards = [SCNBox]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -24,10 +28,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        self.registerGestureRecognizers()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +40,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingSessionConfiguration()
-        
+        configuration.planeDetection = .horizontal
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -47,34 +52,104 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    private func registerGestureRecognizers() {
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(makeTable))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    @objc func makeTable(recognizer :UIGestureRecognizer) {
+        let sceneView = recognizer.view as! ARSCNView
+        let touch = recognizer.location(in: sceneView)
+        
+        let hitResults = sceneView.hitTest(touch, options: [:])
+        
+        if !hitResults.isEmpty {
+            guard let firstHitResult = hitResults.first else {
+                return
+            }
+            let material = SCNMaterial()
+            material.diffuse.contents = UIColor(red: 31/255, green: 93/255, blue: 70/255, alpha: 1.0)
+            guard let firstPlane = self.planes.first else {
+                return
+            }
+            firstPlane.planeGeometry.materials = [material]
+            sceneView.delegate = nil
+            self.planes.removeAll()
+            self.planes.append(firstPlane)
+            self.initDeck(plane: firstPlane)
+        }
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    func initDeck(plane: OverlayPlane){
+        var index: Int = 0
+        while index < 52 {
+            let card: SCNBox = SCNBox(width: 0.06350, height: 0.01, length: 0.0889, chamferRadius: 0.0)
+            let material = SCNMaterial()
+            material.diffuse.contents = UIImage(named: "card_background")
+            card.materials = [ SCNMaterial(), SCNMaterial(), SCNMaterial(), SCNMaterial(), material, SCNMaterial()]
+            let boxNode = SCNNode(geometry: card)
+            boxNode.position = SCNVector3(plane.worldPosition.x,plane.worldPosition.y + Float(index / 100), plane.worldPosition.z)
+            self.sceneView.scene.rootNode.addChildNode(boxNode)
+            index += 1
+        }
+    }
+    
+    @objc func putDownBox(recognizer :UIGestureRecognizer) {
+        
+        let sceneView = recognizer.view as! ARSCNView
+        let touchLocation = recognizer.location(in: sceneView)
+        
+        let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+        
+        if !hitTestResult.isEmpty {
+            
+            guard let hitResult = hitTestResult.first else {
+                return
+            }
+            addBox(hitResult :hitResult)
+        }
+    }
+    
+    private func addBox(hitResult :ARHitTestResult) {
+        
+        let boxGeometry = SCNBox(width: 0.2, height: 0.2, length: 0.1, chamferRadius: 0)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        boxGeometry.materials = [material]
+        
+        let boxNode = SCNNode(geometry: boxGeometry)
+        
+        boxNode.position = SCNVector3(hitResult.worldTransform.columns.3.x,hitResult.worldTransform.columns.3.y + Float(boxGeometry.height/2), hitResult.worldTransform.columns.3.z)
+        
+        self.sceneView.scene.rootNode.addChildNode(boxNode)
         
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         
+        if !(anchor is ARPlaneAnchor) {
+            return
+        }
+        
+        let plane = OverlayPlane(anchor: anchor as! ARPlaneAnchor)
+        self.planes.append(plane)
+        node.addChildNode(plane)
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         
+        let plane = self.planes.filter { plane in
+            return plane.anchor.identifier == anchor.identifier
+            }.first
+        
+        if plane == nil {
+            return
+        }
+        
+        plane?.update(anchor: anchor as! ARPlaneAnchor)
     }
+    
 }
